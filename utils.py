362a64e70850c222390acc7b81fa1c65af60ce61
Fritz Lekschas <service@lekschas.de>
2020-01-17 19:45:28 -0500

    Helper function for natural sorting

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
+import numpy as np
 import pathlib
+import re
 import requests
 
 
@@ -43,3 +45,9 @@ def download_file(
     print(" done!")
 
     return filepath
+
+
+def natural_sort(l):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
+    return sorted(l, key = alphanum_key)
